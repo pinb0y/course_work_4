4694e5a72881266e31001e54ac27586d01c1097d
Pinchuk Sergey <Pinboyer@gmail.com>
2024-06-19 06:34:47 +0300

    make utilities functions

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+from src.classes.vacancy import Vacancy
+
+
+def sort_vacancies(vacancies: list[Vacancy]) -> list[Vacancy]:
+    """
+    Сортирует вакансии по увеличению зарплат "от".
+    :param vacancies: Список объектов класса вакансия.
+    :return: Отсортированный список объектов класса вакансия.
+    """
+
+    return sorted(vacancies)
+
+
+def print_top_n_vacancies(quantity: int, vacancies: list[Vacancy]) -> None:
+    """
+    Выводит на экран заданное пользователем количество вакансий.
+    :param quantity: Количество вакансий
+    :param vacancies: Список объектов класса вакансия.
+    :return: None
+    """
+
+    for i in range(quantity):
+        print(vacancies[i])
+
+
+def vacancy_salary_filter(vacancies: list[Vacancy], salary_range: str) -> list[Vacancy]:
+    """
+    Фильтрует список объектов класса вакансия, согласно заданному диапазону зарплат.
+    :param vacancies: Список объектов класса вакансия.
+    :param salary_range: Заданный пользователем диапазон зарплат.
+    :return: Отфильтрованный список объектов класса вакансия.
+    """
+
+    salary_range_list: list[int] = [int(num.strip()) for num in salary_range.split("-")]
+    filtered_vacancies_list: list = []
+    for vacancy in vacancies:
+        if salary_range_list[0] <= vacancy.salary_from <= salary_range_list[1]:
+            filtered_vacancies_list.append(vacancy)
+    return filtered_vacancies_list
